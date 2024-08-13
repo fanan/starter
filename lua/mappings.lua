71642c37f32219ea2662d85a97f15b60263edc20
fanan <fanan1985@gmail.com>
2024-08-13 23:41:32 +0800

    my init

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,7 +4,31 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--
+local nomap = vim.keymap.del
+nomap("i", "<C-b>")
+nomap("i", "<C-e>")
+nomap("i", "<C-h>")
+nomap("i", "<C-l>")
+nomap("i", "<C-j>")
+nomap("i", "<C-k>")
+
+nomap("n", "<C-h>")
+nomap("n", "<C-l>")
+nomap("n", "<C-j>")
+nomap("n", "<C-k>")
+nomap("n", "<C-s>")
+nomap("n", "<C-c>")
+
+nomap("n", "<leader>n")
+nomap("n", "<leader>rn")
+nomap("n", "<leader>/")
+nomap("n", "<leader>h")
+nomap("n", "<leader>v")
+
+nomap("v", "<leader>/")
+
